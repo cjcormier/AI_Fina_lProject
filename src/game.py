0e7d2342748a8943ffd16729c98bb633b93ee46d
Chris Cormier <chris.hw.cormier@gmail.com>
2017-03-10 13:00:05 -0500

    Expanded logging function by adding specific log functions.

--- src/game.py
+++ src/game.py
@@ -8,13 +8,14 @@ from src.cards import Cards
 
 
 class Game:
-    def __init__(self, num_players, allow_shoots=False):
+    def __init__(self, num_players, allow_shoots=False, log=None):
         self.num_players = num_players
         self.players = self.assign_roles()
         self.allow_shoots = allow_shoots
         self.board = Board()
         self.deck = Deck(6, 11)
         self.winner = BoardStates.NORMAL
+        self.log = log if type(log) is Log else Log()
 
         self.prev_pres = -1
         self.president_name = -1
@@ -22,33 +23,21 @@ class Game:
         self.chancellor = None
 
         self.anarchies = 0
-        self.l_pres = 0
-        self.f_pres = 0
-        self.h_pres = 0
-        self.l_chanc = 0
-        self.f_chanc = 0
-        self.h_chanc = 0
 
     def run(self):
         rounds = 0
         while self.winner is BoardStates.NORMAL:
             rounds += 1
+            Log.log_new_round()
             self.round()
-        message = '\n{} rounds:\n{} liberal presidents, {} fascist presidents and {} hitler ' \
-                  'presidents\n{} liberal chancellors, {} fascist chancellors and {} hitler ' \
-                  'chancellors.'
-        Log.log(message.format(rounds, self.l_pres, self.f_pres, self.h_pres, self.l_chanc,
-                               self.f_chanc, self.h_chanc))
+        self.log.log_game_results(rounds)
 
         return self.winner, self. board.fascist_board, self.board.liberal_board, self.anarchies
 
     def round(self):
-        Log.log('\n------------------------\n\nNew Round')
-
         if self.elect_new_gov():
-            self.record_gov()
+            self.log.record_gov(self.president, self.chancellor)
             self.prev_pres = self.president_name
-            Log.log("Vote Passed")
             if self.check_hitler_chanc_win():
                 return
             next_policy = self.choose_policy()
@@ -56,9 +45,9 @@ class Game:
         else:
             next_policy = self.deck.draw()
             self.anarchies += 1
-            Log.log('Anarchy!!!!!')
+            Log.log_anarchy()
         self.winner = self.board.increment_board(next_policy)
-        Log.log('Next Policy:', next_policy)
+        Log.log_next_policy(next_policy)
         self.shoot(next_policy)
 
     def elect_new_gov(self):
@@ -69,12 +58,8 @@ class Game:
             self.next_pres()
             self.president, self.chancellor = self.propose_gov()
 
-            message = "Voting on new government. President: {} Chancellor: {} ({},{})"
-            Log.log(message.format(self.president_name, self.chancellor.name, self.president.role,
-                                   self.chancellor.role))
             vote_passed = self.vote()
-            if not vote_passed:
-                Log.log('Vote Failed, number of consecutive failed votes:', rounds_of_voting)
+            Log.log_vote_results(vote_passed, rounds_of_voting)
         return vote_passed
 
     def next_pres(self):
@@ -86,9 +71,10 @@ class Game:
     def propose_gov(self):
         players = self.players.keys()
         chancellor_name = self.chancellor.name if self.chancellor is not None else -1
+
         names = [x for x in players if x is not chancellor_name]
         names = [x for x in names if x is not self.prev_pres] if len(self.players) > 5 else names
-        Log.log('Valid chancellors: {}'.format(names))
+        Log.log_valid_chancellors(names)
 
         president = self.players[self.president_name]
         chancellor_name = president.choose_chancellor(names)  # limit valid players
@@ -96,8 +82,8 @@ class Game:
         return president, chancellor
 
     def check_hitler_chanc_win(self):
-        if 4 <= self.board.fascist_board <= 5 and self.chancellor.role is Role.HITLER:
-            Log.log('Chancellor {} is Hitler, fascists win!'.format(self.chancellor.name))
+        Log.log_elected_chancellor(self.chancellor)
+        if 4 <= self.board.fascist_board and self.chancellor.role is Role.HITLER:
             self.winner = BoardStates.HITLER_CHANCELLOR
             return True
         return False
@@ -107,42 +93,23 @@ class Game:
         policies = self.deck.draw_hand()
         p_pick = self.president.president_pick(self.chancellor.name, list(policies))
         c_pick = self.chancellor.chancellor_pick(self.president.name, list(p_pick), remaining)
-        message = '\nDrawn Cards:{}\nPres Pick: {}, Canc Pick: {}\n'
-        Log.log(message.format(policies, p_pick, c_pick))
+        Log.log_policy(policies, p_pick, c_pick)
 
         self.president.analyze_chancellor_card(self.chancellor.name, p_pick, c_pick)
         self.deck.discard(policies, c_pick)
         return c_pick
 
     def vote(self):
-        votes = 0
         ja = []
         nay = []
         for name, player in self.players.items():
             player_vote = player.vote(self.president.name, self.chancellor.name)
             if player_vote:
-                votes += 1
                 ja.append(player.name)
             else:
                 nay.append(player.name)
-        message = 'Votes in favor: {}, Votes against: {}  ({},{})'
-        Log.log(message.format(ja, nay, votes, len(self.players) - votes))
-        return votes >= (len(self.players) / 2)
-
-    def record_gov(self):
-        if self.president.role is Role.LIBERAL:
-            self.l_pres += 1
-        elif self.president.role is Role.FASCIST:
-            self.f_pres += 1
-        elif self.president.role is Role.HITLER:
-            self.h_pres += 1
-
-        if self.chancellor.role is Role.LIBERAL:
-            self.l_chanc += 1
-        elif self.chancellor.role is Role.FASCIST:
-            self.f_chanc += 1
-        elif self.chancellor.role is Role.HITLER:
-            self.h_chanc += 1
+        Log.log_votes(self.president, self.chancellor, ja, nay)
+        return len(ja) >= (len(self.players) / 2)
 
     def analyze_revealed_card(self, next_policy):
         for name, player in self.players.items():
@@ -153,15 +120,10 @@ class Game:
         fascist_board = self.board.fascist_board
         if 4 <= fascist_board <= 5 and next_policy is Cards.FASCIST and self.allow_shoots:
             player_shot = self.president.shoot()
-            Log.log('President {} shot player {}.'.format(self.president.name, player_shot))
+            Log.log_shot_players(self.president, self.players[player_shot])
             if self.players[player_shot].role is Role.HITLER:
-                Log.log('Player {} is Hitler, liberals win!'.format(player_shot))
                 self.winner = BoardStates.HITLER_SHOT
                 return False
-            else:
-                message = 'Player {0} is not Hitler. ({0} was {1} instead and {2} was {3}.)'
-                Log.log(message.format(player_shot, self.players[player_shot].role,
-                                       self.president.name, self.president.role))
             self.remove_player(player_shot)
         return True
 
--- src/logging.py
+++ src/logging.py
@@ -1,7 +1,17 @@
+from src.roles import Role
+
+
 class Log:
     can_log = True
     can_log_probs = False
 
+    def __init__(self):
+        self.l_pres = 0
+        self.f_pres = 0
+        self.h_pres = 0
+        self.l_chanc = 0
+        self.f_chanc = 0
+        self.h_chanc = 0
 
     @staticmethod
     def allow_logging(new_log):
@@ -24,5 +34,91 @@ class Log:
             string += ' '
         if Log.can_log:
             print(string[0:-1])
+
+    def record_gov(self, president, chancellor):
+        if president.role is Role.LIBERAL:
+            self.l_pres += 1
+        elif president.role is Role.FASCIST:
+            self.f_pres += 1
+        elif president.role is Role.HITLER:
+            self.h_pres += 1
+
+        if chancellor.role is Role.LIBERAL:
+            self.l_chanc += 1
+        elif chancellor.role is Role.FASCIST:
+            self.f_chanc += 1
+        elif chancellor.role is Role.HITLER:
+            self.h_chanc += 1
+
+    def log_game_results(self, rounds):
+        message = '\n{} rounds:\n{} liberal presidents, {} fascist presidents and {} hitler ' \
+                  'presidents\n{} liberal chancellors, {} fascist chancellors and {} hitler ' \
+                  'chancellors.'
+        message = message.format(rounds, self.l_pres, self.f_pres, self.h_pres, self.l_chanc,
+                                 self.f_chanc, self.h_chanc)
+        Log.log(message)
+
+    @staticmethod
+    def log_new_round():
+        message = '\n------------------------\nNew Round\n'
+        Log.log(message)
+
+    @staticmethod
+    def log_anarchy():
+        message = 'Anarchy!!!!!\n'
+        Log.log(message)
+
+    @staticmethod
+    def log_next_policy(policy):
+        message = 'Next Policy: {}'
+        Log.log(message.format(policy))
+
+    @staticmethod
+    def log_votes(president, chancellor, ja, nay):
+        message = 'Voting on new government. President: {} Chancellor: {} ({},{})'
+        Log.log(message.format(president.name, chancellor.name, president.role, chancellor.role))
+        message = 'Votes in favor: {}, Votes against: {}  ({},{})'
+        Log.log(message.format(ja, nay, len(ja), len(nay)))
+
+    @staticmethod
+    def log_vote_results(vote_passed, rounds):
+        if vote_passed:
+            message = 'Vote Passed'
+        else:
+            message = 'Vote Failed, number of consecutive failed votes:'
+        Log.log(message.format(rounds))
+
+    @staticmethod
+    def log_valid_chancellors(valid_chancellors):
+        Log.log('Valid chancellors: {}'.format(valid_chancellors))
+
+    @staticmethod
+    def log_elected_chancellor(chancellor):
+        message = 'Late game Fascism; if the chancellor is Hitler, the Fascists win.'
+        Log.log(message)
+        if chancellor.role is Role.HITLER:
+            message = 'Chancellor {} is Hitler, fascists win!'.format(chancellor.name)
+        else:
+            message = 'Chancellor {} not is Hitler, you live to play another day!'
+            message = message.format(chancellor.name)
+        Log.log(message)
+
+    @staticmethod
+    def log_policy(drawn_policies, p_pick, c_pick):
+        message = '\nDrawn Cards:{}\nPres Pick: {}, Canc Pick: {}'
+        Log.log(message.format(drawn_policies, p_pick, c_pick))
+
+    @staticmethod
+    def log_shot_players(president, player_shot):
+        ps_name = player_shot.name
+        message = 'President {} shot player {}.'.format(president.name, ps_name)
+        Log.log(message)
+        if player_shot.role is Role.HITLER:
+            message = 'Player {} is Hitler, liberals win!'.format(ps_name)
+        else:
+            message = 'Player {0} is not Hitler. ({0} was {1} instead and {2} was {3}.)'
+            message = message.format(ps_name, player_shot.role, president.name, president.role)
+        Log.log(message)
+
 
 __all__ = ['Log']
